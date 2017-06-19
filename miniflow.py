5c3e7dcb692a3491fb697e892cbed7bbdbaef21b
Yummy Bian <yummy.bian@gmail.com>
2017-06-19 08:26:27 +0800

    update

--- miniflow.py
+++ miniflow.py
@@ -64,22 +64,6 @@ class Add(Node):
 
 
 class Linear(Node):
-    def __init__(self, inputs, weights, bias):
-        Node.__init__(self, [inputs, weights, bias])
-
-        # NOTE: The weights and bias properties here are not
-        # numbers, but rather references to other nodes.
-        # The weight and bias values are stored within the
-        # respective nodes.
-
-    def forward(self):
-        """
-        Set self.value to the value of the linear function output.
-        """
-        self.value = np.dot(self.inbound_nodes[0].value, self.inbound_nodes[1].value) + self.inbound_nodes[2].value
-
-
-class Linear(Node):
     def __init__(self, X, W, b):
         # Notice the ordering of the input nodes passed to the
         # Node constructor.
@@ -90,6 +74,61 @@ class Linear(Node):
         Set the value of this node to the linear transform output.
         """
         self.value = np.dot(self.inbound_nodes[0].value, self.inbound_nodes[1].value) + self.inbound_nodes[2].value
+
+
+class Sigmoid(Node):
+    """
+    You need to fix the `_sigmoid` and `forward` methods.
+    """
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+        """
+        return 1. / (1. + np.exp(-x))
+
+    def forward(self):
+        """
+        Set the value of this node to the result of the
+        sigmoid function, `_sigmoid`.
+        """
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
+
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+
+        diff = y - a
+        self.value = np.mean(diff**2)
 
 
 def topological_sort(feed_dict):
@@ -134,19 +173,31 @@ def topological_sort(feed_dict):
     return L
 
 
-def forward_pass(output_node, sorted_nodes):
+# def forward_pass(output_node, sorted_nodes):
+#     """
+#     Performs a forward pass through a list of sorted nodes.
+#
+#     Arguments:
+#
+#         `output_node`: A node in the graph, should be the output node (have no outgoing edges).
+#         `sorted_nodes`: A topologically sorted list of nodes.
+#
+#     Returns the output Node's value
+#     """
+#
+#     for n in sorted_nodes:
+#         n.forward()
+#
+#     return output_node.value
+
+def forward_pass(graph):
     """
-    Performs a forward pass through a list of sorted nodes.
+    Performs a forward pass through a list of sorted Nodes.
 
     Arguments:
 
-        `output_node`: A node in the graph, should be the output node (have no outgoing edges).
-        `sorted_nodes`: A topologically sorted list of nodes.
-
-    Returns the output Node's value
+        `graph`: The result of calling `topological_sort`.
     """
-
-    for n in sorted_nodes:
+    # Forward pass
+    for n in graph:
         n.forward()
-
-    return output_node.value
